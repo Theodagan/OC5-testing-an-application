22213d0d91d3305fad80275e1cb5a28716893cfb
theo.perotti <theo.perotti@gmail.com>
2025-05-07 13:46:01 +0000

    [UPDATE]

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,20 @@
 package com.openclassrooms.starterjwt;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {}
 
+    @Test
+    void testMainMethod_runsWithoutException() {
+        assertDoesNotThrow(() ->
+            SpringBootSecurityJwtApplication.main(new String[] {})
+        );
+    }
 }
